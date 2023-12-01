5d1657d964cfcc5c6583fb346ff5ce861d7411bd
viet-ho <103388731+viet-ho@users.noreply.github.com>
2023-11-30 22:11:24 -0700

    Added a convolve function to convolve two signals, producing an output signal.

--- convolve.cpp
+++ convolve.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void convolve(double x[], int N, double h[], int M, double y[], int P);
+
 WAVEFile *inputfile = new WAVEFile();
 WAVEFile *IRfile = new WAVEFile();
 
@@ -20,5 +22,31 @@ int main(int argc, char* argv[]){
 
     inputfile->readWAVEFile(inputFileName);
     IRfile->readWAVEFile(IRFileName);
+
+    cout << "\nInput Size: " << inputfile->signalSize << ", Impulse Size: " << IRfile->signalSize << '\n';
+
+
+}
+
+void convolve(double x[], int N, double h[], int M, double y[], int P) {
+
+    int n, m;
+
+    if (P != (N + M - 1)) {
+        printf("Output signal vector is the wrong size\n");
+        printf("It is %-d, but should be %-d\n", P, (N + M - 1));
+        printf("Aborting convolution\n");
+        return;
+    }
+
+    for (n = 0; n < P; n++) {
+        y[n] = 0.0;
+    }
+
+    for (n = 0; n < N; n++) {
+        for (m = 0; m < M; m++) {
+            y[n+m] += x[n] * h[m];
+        }
+    }
 
 }
